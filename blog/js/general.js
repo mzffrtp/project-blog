a7df8ab915895e14783e88c77e73584afb235ff5
muzaffer <115178224+mzffrtp@users.noreply.github.com>
2023-01-09 11:10:58 +0100

    Delete blog directory

--- blog/js/general.js
+++
@@ -1,8 +0,0 @@
-/* Blog Date Start*/
-const blogDate = document.getElementById("blogDate")
-
-const nowdate = new Date()
-
-blogDate.innerText = nowdate.toLocaleDateString().replaceAll("/",".")
-
-/* Blog Date End*/
